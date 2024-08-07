a0bdd316fddb4c558dca80cd65ee910992e63da3
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-08-07 23:55:55 +0700

    added material design icons, styled the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,39 @@
 import { Link, useParams } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import Icon from "@mdi/react";
+import { mdiShoppingOutline, mdiMagnify } from "@mdi/js";
 
 function Navbar() {
   return (
     <div className={styles.Navbar}>
-      <div className="title">
-        <Link to="home" className={styles.Title}>
-          Eric's
-        </Link>
+      <div className={styles.navbarLeft}>
+        <div className={styles.Title}>
+          <Link to="home">eric's</Link>
+        </div>
+        <div className={styles.navLink}>
+          <Link to="home">Home</Link>
+        </div>
+        <div className={styles.navLink}>
+          <Link to="store">Store</Link>
+        </div>
       </div>
-      <Link to="home">Home</Link>
-      <Link to="store">Store</Link>
+      <div className={styles.navbarRight}>
+        <div className="searchBar">
+          <Icon path={mdiMagnify} size={1} className={styles.Icon} />
+          <input
+            type="text"
+            className={styles.searchBar}
+            id="searchBar"
+            placeholder="Search"
+          />
+        </div>
 
-      <input
-        type="text"
-        className="searchBar"
-        id="searchBar"
-        placeholder="Search"
-      />
-      <Link to="bag">Bag</Link>
+        <div className={styles.Bag}>
+          <Link to="bag">
+            <Icon path={mdiShoppingOutline} size={1} />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
