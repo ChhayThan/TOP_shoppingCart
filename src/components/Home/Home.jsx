e2a76e61af8b313b61a65d54528d637fcc71f205
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-08-08 20:50:01 +0700

    Added home content - need to work on item Carousel and call API

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.homeContainer}>
+      <h1 className={styles.homeTitle}>Welcome to eric's fake store.</h1>
+      <p className={styles.homeDescription}>
+        Discover our selection of fake products, random as they come. Delivered
+        straight from nowhere to your thoughts.
+      </p>
+      <button className={styles.shopBtn}>
+        <Link to="store">Shop Now</Link>
+      </button>
+
+      <p>Carousel here...</p>
+    </div>
+  );
 }
 
 export default Home;
