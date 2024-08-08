1e297b8b67e73420994b7f5484a0a9f370e596c8
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-08-08 22:24:15 +0700

    Completed Data fetching + created home item cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products", {
+      mode: "cors",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Server error");
+        }
+        return response.json();
+      })
+      .then((response) => setData(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="error">
+        <p>Network error encountered...</p>
+      </div>
+    );
+  }
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Outlet context={[data]} />
     </>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
 function Home() {
+  const [data] = useOutletContext();
+
+  const navigate = useNavigate();
+  function handleItemClick(itemTitle) {
+    navigate(`../store/${itemTitle}`);
+  }
   return (
     <div className={styles.homeContainer}>
       <h1 className={styles.homeTitle}>Welcome to eric's fake store.</h1>
@@ -9,11 +17,34 @@ function Home() {
         Discover our selection of fake products, random as they come. Delivered
         straight from nowhere to your thoughts.
       </p>
-      <button className={styles.shopBtn}>
-        <Link to="store">Shop Now</Link>
-      </button>
+      <Link to="../store" className={styles.shopLink}>
+        Shop Now
+      </Link>
+      <p className={styles.featuredItems}>Featured Items</p>
+      <div className={styles.itemContainer}>
+        {data.map((object, index) => {
+          if (index > 3) {
+            return;
+          }
+          return (
+            <div
+              className={styles.item}
+              key={object.id}
+              onClick={() => handleItemClick(object.title)}
+            >
+              <img
+                src={object.images[0]}
+                alt={object.title}
+                className={styles.itemImg}
+                width="200"
+              />
+              <p className={styles.itemTitle}>{object.title}</p>
+            </div>
+          );
+        })}
+      </div>
 
-      <p>Carousel here...</p>
+      <div className={styles.footer}>Created by Eric C.</div>
     </div>
   );
 }
